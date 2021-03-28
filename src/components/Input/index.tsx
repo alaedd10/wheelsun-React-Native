0a50eeeda189fa9c5f71912fda306cae1e2b2ef2
unknown <alaedd@gmail.com>
2021-03-28 23:51:16 +0100

    fixing SignIn screen + icons + splash

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,47 +1,48 @@
-import React from 'react'
-import { View, Text, StyleSheet, TextInput } from 'react-native'
-import { Colors, Values } from '../../constants'
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Colors, Values } from "../../constants";
 type inputProps = {
-    label: string,
-    placeholder: string,
-    isPassword: boolean
-}
-const input = ({ label, placeholder, isPassword }: inputProps) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.label}>{label}:</Text>
-            <TextInput
-                style={styles.input}
-                placeholder={placeholder}
-                secureTextEntry={isPassword}
-            />
-        </View>
-    )
-}
+  label: string;
+  placeholder?: string;
+  isPassword?: boolean;
+};
+const input = ({ label, placeholder = "", isPassword = false }: inputProps) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        secureTextEntry={isPassword}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center'
-    },
-    label: {
-        fontSize: Values.labelTextSize,
-        color: Colors.textColor,
-        fontWeight: 'bold',
-        marginVertical: 10,
-        alignSelf: "flex-start"
-    },
-    input: {
-        elevation: 8,
-        borderRadius: 10,
-        paddingVertical: 10,
-        paddingHorizontal: 12,
-        width: '80%',
-        height: 50,
-        backgroundColor: Colors.inputColors
-    }
-})
+  container: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  label: {
+    fontSize: Values.labelTextSize,
+    color: Colors.textColor,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    marginVertical: 10,
+    alignSelf: "flex-start",
+  },
+  input: {
+    // elevation: 8,
+    borderRadius: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    borderColor: Colors.inputBorderColor,
+    borderWidth: 0.5,
+    width: "100%",
+    height: 50,
+    backgroundColor: Colors.inputBgColor,
+  },
+});
 
-
-export default input
+export default input;
--- src/constants/Colors.tsx
+++ src/constants/Colors.tsx
@@ -2,5 +2,6 @@ export default {
     buttonTextColor: 'white',
     buttonBgColor: '#DD8522',
     textColor: '#47545C',
-    inputColors: '#F7F8F9'
+    inputBgColor: '#F7F8F9',
+    inputBorderColor: '#D5DDE0',
 }
